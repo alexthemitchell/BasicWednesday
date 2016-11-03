444026fc9ee8f7d305232b23491f588f5e8e0e14
Alex Mitchell <alex@alexthemitchell.com>
2016-11-02 20:14:11 -0500

    Initial Commit

---
+++ Ticker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ticker
+//
+//  Created by Alex Mitchell on 11/2/16.
+//  Copyright © 2016 Grinnell AppDev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
