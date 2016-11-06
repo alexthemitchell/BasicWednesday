4a99f99931998afc372ff16e111d90e1b41994d5
Alex Mitchell <alex@alexthemitchell.com>
2016-11-06 15:28:10 -0600

    Add code from Wednesday

--- Ticker/ViewController.m
+++ Ticker/ViewController.m
@@ -1,18 +1,23 @@
-//
-//  ViewController.m
-//  Ticker
-//
-//  Created by Alex Mitchell on 11/2/16.
-//  Copyright © 2016 Grinnell AppDev. All rights reserved.
-//
-
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *firstNameField;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameField;
+@property (weak, nonatomic) IBOutlet UITextField *yearField;
+
+@property (weak, nonatomic) IBOutlet UILabel *outputLabel;
 
 @end
 
 @implementation ViewController
+- (IBAction)didTapButton:(id)sender {
+  _outputLabel.text = [self firstName:_firstNameField.text lastName:_lastNameField.text year:_yearField.text];
+}
+
+- (NSString *)firstName:(NSString *)fn lastName:(NSString *)ln year:(NSString *)year {
+  return [NSString stringWithFormat:@"%@ %@ %@", fn, ln, [NSString stringWithFormat:@"'%@",[year substringFromIndex:2]]];
+  
+}
 
 - (void)viewDidLoad {
   [super viewDidLoad];
